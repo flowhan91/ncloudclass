0ce54ad7d4195646f4ac3bf4ec50c2126354f1f8
flowhan91 <flowhan91@gmail.com>
2025-11-20 12:52:59 +0900

    commit

--- SpringBoot2/src/main/java/com/lgdx/web/SpringBoot2Application.java
+++ SpringBoot2/src/main/java/com/lgdx/web/SpringBoot2Application.java
@@ -3,6 +3,8 @@ package com.lgdx.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 //@SpringBootApplication : 해당 클래스의 위치 기준으로 하위에 있는 파일을 읽어
 //Spring Boot의 설정을 자동적으로 진행하는 클래스 이다. 
@@ -11,7 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.lgdx.mapper")
-public class SpringBoot2Application {
+public class SpringBoot2Application extends SpringBootServletInitializer{
+	
+	//내장 톰캣이 아닌 외부 톰캣을 사용하기 위한 설정 (.jsp)
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBoot2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot2Application.class, args);
